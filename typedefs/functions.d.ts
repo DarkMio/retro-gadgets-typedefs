077a595544eaad2cac3c8ef86ed73552065054c4
NexTre-dev <66027449+NexTre-dev@users.noreply.github.com>
2022-12-02 17:40:57 +0100

    fix: move global defs to `declare global`

--- typedefs/functions.d.ts
+++ typedefs/functions.d.ts
@@ -1,79 +1,95 @@
 /**
  * Imports
  */
-import { IntRange } from "./helpers";
-import { color, ANSIColors, Desk, PowerButton } from "./types";
+import { IntRange } from './helpers';
+import { color, ANSIColors, Desk, PowerButton } from './types';
 
 /*
  * Global Methods
  */
+declare global {
+	/**
+	 * Compose and returns a RGB Color object. Values for the 3 channels are always expressed in the range 0-255.
+	 */
+	function Color(
+		this: void,
+		r: IntRange<0, 256>,
+		g: IntRange<0, 256>,
+		b: IntRange<0, 256>,
+	): color;
+	/**
+	 * Compose and returns a RGB Color object, with Alpha. Values for the 4 channels are always expressed in the range 0-255. Alpha 0 is transparent
+	 */
+	function ColorRGBA(
+		this: void,
+		r: IntRange<0, 256>,
+		g: IntRange<0, 256>,
+		b: IntRange<0, 256>,
+		a: IntRange<0, 256>,
+	): color;
+	/**
+	 * Compose and returns a RGB Color Object, expressing it in HSV values.
+	 * @param h [0-360]
+	 * @param s [0-100]
+	 * @param v [0-100]
+	 */
+	function ColorHSV(
+		this: void,
+		h: IntRange<0, 361>,
+		s: IntRange<0, 101>,
+		v: IntRange<0, 101>,
+	): color;
 
-/**
- * Compose and returns a RGB Color object. Values for the 3 channels are always expressed in the range 0-255.
- */
-declare function Color(this: void, r: IntRange<0, 256>, g: IntRange<0, 256>, b: IntRange<0, 256>): color
-/**
- * Compose and returns a RGB Color object, with Alpha. Values for the 4 channels are always expressed in the range 0-255. Alpha 0 is transparent
- */
-declare function ColorRGBA(this: void, r: IntRange<0, 256>, g: IntRange<0, 256>, b: IntRange<0, 256>, a: IntRange<0, 256>): color
-/**
- * Compose and returns a RGB Color Object, expressing it in HSV values.
- * @param h [0-360] 
- * @param s [0-100]
- * @param v [0-100]
- */
-declare function ColorHSV(this: void, h: IntRange<0, 361>, s: IntRange<0, 101>, v: IntRange<0, 101>): color
+	function log(this: void, message: string): void;
+	function logWarning(this: void, message: string): void;
+	function logError(this: void, message: string): void;
 
+	function write(this: void, text: string): void;
+	function writeLn(this: void, text: string): void;
 
-declare function log(this: void, message: string): void
-declare function logWarning(this: void, message: string): void
-declare function logError(this: void, message: string): void
+	/**
+	 * Use ANSI Colors
+	 */
+	function setFgColor(this: void, colorId: ANSIColors): void;
+	/**
+	 * Use ANSI colors
+	 */
+	function setBgColor(this: void, colorId: ANSIColors): void;
+	function resetFgColor(this: void): void;
+	function resetBgColor(this: void): void;
+	function resetColors(this: void): void;
 
-declare function write(this: void, text: string): void
-declare function writeLn(this: void, text: string): void
+	function setCurorPos(this: void, column: number, line: number): void;
+	function setCurorX(this: void, column: number): void;
+	function setCursorY(this: void, line: number): void;
+	function moveCursorX(this: void, deltaColumn: number): void;
+	function moveCursorY(this: void, deltaLine: number): void;
+	function saveCurosorPos(this: void): void;
+	function restoreCursorPos(this: void): void;
 
-/**
- * Use ANSI Colors
- */
-declare function setFgColor(this: void, colorId: ANSIColors): void
-/**
- * Use ANSI colors
- */
-declare function setBgColor(this: void, colorId: ANSIColors): void
-declare function resetFgColor(this: void): void
-declare function resetBgColor(this: void): void
-declare function resetColors(this: void): void
+	function clear(this: void): void;
+	function clearToEndLine(this: void): void;
 
-declare function setCurorPos(this: void, column: number, line: number): void
-declare function setCurorX(this: void, column: number): void
-declare function setCursorY(this: void, line: number): void
-declare function moveCursorX(this: void, deltaColumn: number): void
-declare function moveCursorY(this: void, deltaLine: number): void
-declare function saveCurosorPos(this: void): void
-declare function restoreCursorPos(this: void): void
+	/*
+	 * Desk Methods
+	 */
+	const desk: Desk;
 
-declare function clear(this: void): void
-declare function clearToEndLine(this: void): void
-
-/*
- * Desk Methods
- */
-declare const desk: Desk
-
-/**
- * Runtime functions
- */
-declare let update: () => void;
+	/**
+	 * Runtime functions
+	 */
+	let update: () => void;
+}
 
 /*
  * Undocumented
  */
 
-type MultitoolConnector = { }
+type MultitoolConnector = {};
 
 type Gadget = {
-    MultitoolConnector: MultitoolConnector,
-    PowerButton: PowerButton,
-    // the following can be any number of components with their name and number attached
-    [k: string]: any
-}
+	MultitoolConnector: MultitoolConnector;
+	PowerButton: PowerButton;
+	// the following can be any number of components with their name and number attached
+	[k: string]: any;
+};
